1f274bbca78a01a3eb86d9a9420081ff855e0e9d
Madina <madinarn@code.edu.az>
2022-08-02 00:51:34 +0400

    DbContext is created

--- DataAccess/Contexts/DbContext.cs
+++ DataAccess/Contexts/DbContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Contexts
 {
-    internal class DbContext
+    static class DbContext
     {
+        static DbContext()
+        {
+            Admins = new List<Admin>();
+            Drugs = new List<Drug>();
+        }
     }
 }
