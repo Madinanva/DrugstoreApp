0b01311b42dd6581e03692979e7ab4f85cef0e09
Madina <madinarn@code.edu.az>
2022-08-04 01:28:09 +0400

    Owner controller prepared

--- DrugstoreApp/Controllers/OwnerController.cs
+++ DrugstoreApp/Controllers/OwnerController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Helpers;
+using DataAccess.Repositories.Implementations;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,146 @@ using System.Threading.Tasks;
 
 namespace Manage.Controllers
 {
-    internal class OwnerController
+    public class OwnerController
     {
+        private OwnerRepository _ownerRepository;
+        public OwnerController()
+        {
+            _ownerRepository = new OwnerRepository();
+        }
+        public void CreateOwner()
+        {
+            ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Enter owner name:");
+            string name = Console.ReadLine();
+            ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Enter owner surname:");
+            string surname = Console.ReadLine();
+            var owner = new Owner
+            {
+                Name = name,
+                Surname = surname
+            };
+
+            var dbOwner = _ownerRepository.Create(owner);
+            if (dbOwner != null)
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, $"{dbOwner.Name} {dbOwner.Surname} is created");
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, $"Something went wrong");
+            }
+        }
+        public void UpdateOwner()
+        {
+            var owners = _ownerRepository.GetAll();
+            if (owners.Count > 0)
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "All owners list:");
+                foreach (var owner in owners)
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, $"Id: {owner.Id}, Fullname: {owner.Name} {owner.Surname}");
+                }
+            Id: ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Enter owner id:");
+                string id = Console.ReadLine();
+                int ownerId;
+                var result = int.TryParse(id, out ownerId);
+                if (result)
+                {
+                    var dbOwner = _ownerRepository.Get(o => o.Id == ownerId);
+                    if (dbOwner != null)
+                    {
+                        string oldName = dbOwner.Name;
+                        string oldSurname = dbOwner.Surname;
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Enter owner's new name");
+                        string newName = Console.ReadLine();
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Enter owner's new surname");
+                        string newSurname = Console.ReadLine();
+                        var updatedOwner = new Owner
+                        {
+                            Id = dbOwner.Id,
+                            Name = newName,
+                            Surname = newSurname
+                        };
+                        _ownerRepository.Update(updatedOwner);
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, $"{oldName} {oldSurname} is updated to {newName} {newSurname}");
+                    }
+                    else
+                    {
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Owner doesn't exist with this id");
+                        goto Id;
+                    }
+                }
+                else
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please enter correct id");
+                    goto Id;
+                }
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "There is no any owner");
+            }
+        }
+        public void DeleteOwner()
+        {
+            var owners = _ownerRepository.GetAll();
+            if(owners.Count() > 0)
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "All owners list");
+                foreach(var owner in owners)
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, $"Id: {owner.Id}, Fullname: {owner.Name} {owner.Surname}");
+                }
+            Id: ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Enter owner id:");
+                string id = Console.ReadLine();
+                int ownerId;
+                var result = int.TryParse(id, out ownerId);
+
+                if (result)
+                {
+                    var owner = _ownerRepository.Get(o => o.Id == ownerId);
+                    if(owner != null)
+                    {
+                        string fullName = $"{owner.Name} {owner.Surname}";
+                        _ownerRepository.Delete(owner);
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Green, $"{fullName} is deleted");
+                    }
+                    else
+                    {
+                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Owner doesn't exist with this id");
+                        goto Id;
+                    }
+                }
+                else
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please enter correct id");
+                    goto Id;
+                }
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "There is no any owner");
+            }
+        }
+        public void GetAllOwners()
+        {
+            var owners = _ownerRepository.GetAll();
+            if(owners.Count > 0)
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "All owners list:");
+                foreach(var owner in owners)
+                {
+                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, $"Id: {owner.Id}, Fullname: {owner.Name} {owner.Surname}");
+                }
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "There is no any owner");
+            }
+        }
+        public void Logout()
+        {
+            ConsoleHelper.WriteTextWithColor(ConsoleColor.Magenta, "Thanks for using this application");
+        }
     }
 }
--- DrugstoreApp/Program.cs
+++ DrugstoreApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using Core.Helpers;
+﻿using Core.Constants;
+using Core.Helpers;
 using Manage.Controllers;
 
 namespace Manage
@@ -8,22 +9,275 @@ namespace Manage
         static void Main()
         {
             AdminController _adminController = new AdminController();
-            DrugController _drugController = new DrugController();
-            DruggistController _druggistController = new DruggistController();
-            DrugstoreController drugstoreController = new DrugstoreController();
-            OwnerController _ownerController = new OwnerController();
 
         Authentication: var admin = _adminController.Authenticate();
             if (admin != null)
             {
                 ConsoleHelper.WriteTextWithColor(ConsoleColor.Yellow, "Welcome");
                 Console.WriteLine("---");
-                while (true)
+              Initial: Console.WriteLine("Please choose one of this options: 0-Owners, 1-Drugstores, 2-Drugs, 3-Druggists");
+                string number = Console.ReadLine();
+                try
                 {
-                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "1- Create Owner");
 
+                    while (true)
+                    {
+                        if (number == "0")
+                        {
+                            OwnerController _ownerController = new OwnerController();
+                            while (true)
+                            {
+
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkMagenta, "Select Option");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "0- Logout");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "1- Create Owner");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "2- Update Owner");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "3- Delete Owner");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "4- Get All Owners");
+
+                                string number1 = Console.ReadLine();
+                                int selectedNumber;
+                                bool result = int.TryParse(number1, out selectedNumber);
+                                if (result)
+                                {
+                                    if (selectedNumber >= 0 && selectedNumber <= 4)
+                                    {
+                                        switch (selectedNumber)
+                                        {
+                                            case (int)FirstOptions.CreateOwner:
+                                                _ownerController.CreateOwner();
+                                                goto Initial;
+                                                break;
+                                            case (int)FirstOptions.UpdateOwner:
+                                                _ownerController.UpdateOwner();
+                                                goto Initial;
+                                                break;
+                                            case (int)FirstOptions.DeleteOwner:
+                                                _ownerController.DeleteOwner();
+                                                goto Initial;
+                                                break;
+                                            case (int)FirstOptions.GetAllOwners:
+                                                _ownerController.GetAllOwners();
+                                                goto Initial;
+                                                break;
+                                            case (int)FirstOptions.Logout:
+                                                _ownerController.Logout();
+                                                goto Initial;
+                                                return;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                    }
+                                }
+                                else
+                                {
+                                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                }
+                            }
+                        }
+                        else if (number == "1")
+                        {
+                            DrugstoreController _drugstoreController = new DrugstoreController();
+                            while (true)
+                            {
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkMagenta, "Select Option");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "0- Logout");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "1- Create Drugstore");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "2- Update Drugstore");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "3- Delete Drugstore");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "4- Get All Drugstores");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "5- Get All Drugstores By Owner");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "6- Sale");
+
+                                string number1 = Console.ReadLine();
+                                int selectedNumber;
+                                bool result = int.TryParse(number1, out selectedNumber);
+                                if (result)
+                                {
+                                    if (selectedNumber >= 0 && selectedNumber <= 6)
+                                    {
+                                        switch (selectedNumber)
+                                        {
+                                            case (int)SecondOptions.CreateDrugstore:
+                                                _drugstoreController.CreateDrugstore();
+                                                goto Initial;
+                                                break;
+                                            case (int)SecondOptions.UpdateDrugstore:
+                                                _drugstoreController.UpdateDrugstore();
+                                                goto Initial;
+                                                break;
+                                            case (int)SecondOptions.DeleteDrugstore:
+                                                _drugstoreController.DeleteDrugstore();
+                                                goto Initial;
+                                                break;
+                                            case (int)SecondOptions.GetAllDrugstores:
+                                                _drugstoreController.GetAllDrugstores();
+                                                goto Initial;
+                                                break;
+                                            case (int)SecondOptions.GetAllDrugstoresByOwner:
+                                                _drugstoreController.GetAllDrugstoresByOwner();
+                                                goto Initial;
+                                                break;
+                                            case (int)SecondOptions.Sale:
+                                                _drugstoreController.Sale();
+                                                goto Initial;
+                                                break;
+                                            case (int)SecondOptions.Logout:
+                                                _drugstoreController.Logout();
+                                                goto Initial;
+                                                return;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                    }
+                                }
+                                else
+                                {
+                                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                }
+                            }
+                        }
+                        else if (number == "2")
+                        {
+                            DrugController _drugController = new DrugController();
+                            while (true)
+                            {
+
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkMagenta, "Select Option");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "0- Logout");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "1- Create Drug");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "2- Update Drug");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "3- Delete Drug");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "4- Get All Drugs");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "5- Get All Drugs By Drugstore");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "6- Filter");
+
+                                string number1 = Console.ReadLine();
+                                int selectedNumber;
+                                bool result = int.TryParse(number1, out selectedNumber);
+                                if (result)
+                                {
+                                    if (selectedNumber >= 0 && selectedNumber <= 6)
+                                    {
+                                        switch (selectedNumber)
+                                        {
+                                            case (int)ThirdOptions.CreateDrug:
+                                                _drugController.CreateDrug();
+                                                goto Initial;
+                                                break;
+                                            case (int)ThirdOptions.UpdateDrug:
+                                                _drugController.UpdateDrug();
+                                                goto Initial;
+                                                break;
+                                            case (int)ThirdOptions.DeleteDrug:
+                                                _drugController.DeleteDrug();
+                                                goto Initial;
+                                                break;
+                                            case (int)ThirdOptions.GetAllDrugs:
+                                                _drugController.GetAllDrugs();
+                                                goto Initial;
+                                                break;
+                                            case (int)ThirdOptions.GetAllDrugsByDrugstore:
+                                                _drugController.GetAllDrugsByDrugstore();
+                                                goto Initial;
+                                                break;
+                                            case (int)ThirdOptions.Filter:
+                                                _drugController.Filter();
+                                                goto Initial;
+                                                break;
+                                            case (int)ThirdOptions.Logout:
+                                                _drugController.Logout();
+                                                goto Initial;
+                                                return;
+
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                    }
+                                }
+                                else
+                                {
+                                    ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                }
+                            }
+                        }
+                        else if (number == "3")
+                        {
+                            DruggistController _druggistController = new DruggistController();
+                            while (true)
+                            {
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.DarkMagenta, "Select Option");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "0- Logout");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "1- Create Druggist");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "2- Update Druggist");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "3- Delete Druggist");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "4- Get All Druggist");
+                                ConsoleHelper.WriteTextWithColor(ConsoleColor.Cyan, "5- Get All Druggist By Drugstore");
+
+                                string number1 = Console.ReadLine();
+                                int selectedNumber;
+                                bool result = int.TryParse(number1, out selectedNumber);
+                                if (result)
+                                {
+                                    if (selectedNumber >= 0 && selectedNumber <= 5)
+                                    {
+                                        switch (selectedNumber)
+                                        {
+                                            case (int)FourthOptions.CreateDruggist:
+                                                _druggistController.CreateDruggist();
+                                                goto Initial;
+                                                break;
+                                            case (int)FourthOptions.UpdateDruggist:
+                                                _druggistController.UpdateDruggist();
+                                                goto Initial;
+                                                break;
+                                            case (int)FourthOptions.DeleteDruggist:
+                                                _druggistController.DeleteDruggist();
+                                                goto Initial;
+                                                break;
+                                            case (int)FourthOptions.GetAllDruggist:
+                                                _druggistController.GetAllDruggist();
+                                                goto Initial;
+                                                break;
+                                            case (int)FourthOptions.GetAllDruggistByDrugstore:
+                                                _druggistController.GetAllDruggistByDrugstore();
+                                                goto Initial;
+                                                break;
+                                            case (int)FourthOptions.Logout:
+                                                _druggistController.Logout();
+                                                goto Initial;
+                                                return;
+                                        }
+                                    }
+                                    else
+                                    {
+                                        ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Please, enter correct number");
+                                    }
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Thanks for visiting");
+                                    break;
+                                }
+                            }
+                        }
+                    }
                 }
-
+                catch
+                {
+                    Console.WriteLine("Thanks for visiting");
+                }
+            }
+            else
+            {
+                ConsoleHelper.WriteTextWithColor(ConsoleColor.Red, "Admin username or password is incorrect");
+                goto Authentication;
             }
         }
     }
